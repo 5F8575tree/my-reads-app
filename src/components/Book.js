f88f8f52b26a1e38bd32589f1bd703a2bbbb82c9
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-05-03 12:25:50 +0900

    ensure props data filters all the way down comps

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,27 +4,24 @@ const Book = ({ Books }) => {
 
   console.log({Books});
 
-  const title = Books.title;
-  const authors = Books.authors;
-
   return (
     <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                        }}
-                      ></div>
-                    </div>
-                    <div className="book-title">{Books[0].title}</div>
-                    <div className="book-authors">{Books[0].authors}</div>
-                  </div>
-                </li>
+        <div className="book">
+            <div className="book-top">
+                <div
+                    className="book-cover"
+                    style={{
+                     width: 128,
+                     height: 193,
+                     backgroundImage:
+                     `url(${Books.url})`
+                  }}
+                  ></div>
+            </div>
+            <div className="book-title">{Books.title}</div>
+            <div className="book-authors">{Books.authors}</div>
+        </div>
+    </li>
   )
 }
 
--- src/components/BookStateTable.js
+++ src/components/BookStateTable.js
@@ -5,12 +5,16 @@ const BookStateTable = ({ Books }) => {
 
     console.log({Books});
 
+    const currentlyReading = Books.filter((book) => book.shelf === "currentlyReading");
+    const wantToRead = Books.filter((book) => book.shelf === "wantToRead");
+    const read = Books.filter((book) => book.shelf === "read");
+
     return (
         <div className="list-books-content">
             <div>
-                <Bookshelf Books={Books} />
-                <Bookshelf Books={Books} />
-                <Bookshelf Books={Books} />
+                <Bookshelf name="Currently Reading" Books={currentlyReading} />
+                <Bookshelf name="Want to Read" Books={wantToRead} />
+                <Bookshelf name="Read" Books={read} />
             </div>
         </div>
     )
