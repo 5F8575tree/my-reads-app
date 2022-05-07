d80ceaf04b7fffec7914ea1fa82d8c9e198492bb
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-05-07 13:22:49 +0900

    get search function working

--- src/components/App.js
+++ src/components/App.js
@@ -8,34 +8,38 @@ import { Routes, Route } from "react-router-dom";
 //TODO: build out the custom hook to replace the changeShelf props that run all the way down.
 
 const App = () => {
-
   const [Books, setBooks] = useState([]);
 
   const updateShelf = useCallback((book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-      setBooks(Books.map(b => b.id === book.id ? { ...book, shelf } : b));
-    }
-    );
-  }, [Books]);
-
+      setBooks(Books.map((b) => (b.id === book.id ? { ...book, shelf } : b)));
+    });
+  }, []);
 
   //we need to use the BooksAPI to getAll() the books within a useEffect
   useEffect(() => {
-      BooksAPI.getAll().then((books) => {
-          setBooks(books);
-      }
-      );
+    BooksAPI.getAll().then((books) => {
+      setBooks(books);
+    });
   }, [updateShelf]);
 
-
+  const updateBooks = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      setBooks(Books.map((b) => (b.id === book.id ? { ...book, shelf } : b)));
+    });
+  };
+  console.log(Books);
   return (
     <Routes>
-      <Route exact path="/" element={
-        <Home Books={Books} updateShelf={updateShelf} />
-      } />
-      <Route path="/search" element={
-        <SearchBooks Books={Books} updateShelf={updateShelf} />}
-        />
+      <Route
+        exact
+        path="/"
+        element={<Home Books={Books} updateShelf={updateBooks} />}
+      />
+      <Route
+        path="/search"
+        element={<SearchBooks Books={Books} updateShelf={setBooks} />}
+      />
     </Routes>
   );
 };
