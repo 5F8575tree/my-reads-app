ec2bc8249f7bb2ad983f48f260e2834e0014caaf
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-06-19 20:20:40 +0900

    update style of background, buttons, header

--- src/components/BookStateTable.js
+++ src/components/BookStateTable.js
@@ -2,20 +2,29 @@ import React from "react";
 import Bookshelf from "./Bookshelf";
 
 const BookStateTable = ({ Books, updateShelf }) => {
+  const currentlyReading = Books.filter(
+    (book) => book.shelf === "currentlyReading"
+  );
+  const wantToRead = Books.filter((book) => book.shelf === "wantToRead");
+  const read = Books.filter((book) => book.shelf === "read");
 
-    const currentlyReading = Books.filter((book) => book.shelf === "currentlyReading");
-    const wantToRead = Books.filter((book) => book.shelf === "wantToRead");
-    const read = Books.filter((book) => book.shelf === "read");
+  return (
+    <div className="list-books-content">
+      <div className="shelf-title">
+        <Bookshelf
+          name="Currently Reading"
+          Books={currentlyReading}
+          updateShelf={updateShelf}
+        />
+        <Bookshelf
+          name="Want to Read"
+          Books={wantToRead}
+          updateShelf={updateShelf}
+        />
+        <Bookshelf name="Read" Books={read} updateShelf={updateShelf} />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="list-books-content">
-            <div>
-                <Bookshelf name="Currently Reading" Books={currentlyReading} updateShelf={updateShelf}/>
-                <Bookshelf name="Want to Read" Books={wantToRead} updateShelf={updateShelf}/>
-                <Bookshelf name="Read" Books={read} updateShelf={updateShelf}/>
-            </div>
-        </div>
-    )
-}
-
-export default BookStateTable
+export default BookStateTable;
